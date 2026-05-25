7297149334a46548a1e51faa635a45392a56e67b
MalikAlsaud <alsaudmalek@gmail.com>
2026-05-25 19:35:36 -0400

    Added the files

---
+++ Vertions/v1/SB/Content/Splash/RandomSplash.bat
@@ -0,0 +1,36 @@
+﻿@echo off
+setlocal EnableDelayedExpansion
+
+set "SCRIPT_DIR=%~dp0"
+set "PATH_DIR=%SCRIPT_DIR%Splashs"
+set "SPLASH_IMG=%SCRIPT_DIR%Splash.png"
+set "cnt=0"
+
+for %%F in ("%PATH_DIR%\*.jpg") do (
+    set "file[!cnt!]=%%~fF"
+    set /a cnt+=1
+)
+for %%F in ("%PATH_DIR%\*.png") do (
+    set "file[!cnt!]=%%~fF"
+    set /a cnt+=1
+)
+
+if %cnt%==0 goto :error
+
+set /a randIdx=%RANDOM% %% %cnt%
+set "selected=!file[%randIdx%]!"
+
+copy /Y "!selected!" "!SPLASH_IMG!" >nul
+if errorlevel 1 goto :error
+
+timeout /t 1 /nobreak >nul
+
+cd /d "%~dp0..\..\.."
+start "" "SB.exe"
+
+exit
+
+:error
+for %%F in ("%SCRIPT_DIR%\*.jpg") do del /f /q "%%~fF"
+for %%F in ("%SCRIPT_DIR%\*.png") do del /f /q "%%~fF"
+exit /b 1
